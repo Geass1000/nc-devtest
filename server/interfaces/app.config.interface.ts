a0937f30659198f19fbf1eba9115cedc8ac6e4d2
Geass <geass1000@gmail.com>
2017-08-24 03:38:53 +0300

    Add: MySQL interface for make connection

--- server/interfaces/app.config.interface.ts
+++ server/interfaces/app.config.interface.ts
@@ -3,6 +3,7 @@ export interface IAppConfig {
 	env ?: string;
 	express ?: ICExpress;
 	mongodb ?: ICMongoDB;
+	mysql ?: ICMySQL;
 	crypto ?: ICCrypro;
 }
 export interface ICExpress {
@@ -12,7 +13,15 @@ export interface ICMongoDB {
 	username : string;
 	password : string;
 	host : string;
-	port : string;
+	port : number;
+	database : string;
+}
+export interface ICMySQL {
+	connectionLimit : number;
+	host : string;
+	port : number;
+	user : string;
+	password : string;
 	database : string;
 }
 export interface ICCrypro {
