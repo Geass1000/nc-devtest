22e1fcd74ca40c6a43e0b4a587670498181b589c
Geass <geass1000@gmail.com>
2017-08-24 03:31:38 +0300

    Add: Creating pool connection to the MySQL

---
+++ server/config/mysql.database.ts
@@ -0,0 +1,9 @@
+import { createPool, Pool } from 'mysql2/promise';
+import { PoolOptions } from 'mysql2';
+
+import { logger } from '../config/logger.config';
+import { config } from './app.config';
+
+logger.info(config.mysql.host, config.mysql.port, config.mysql.user, config.mysql.password, config.mysql.database);
+
+export const pool : Pool = createPool(<PoolOptions>config.mysql);
