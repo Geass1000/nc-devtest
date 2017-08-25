febda12d2337d94013aadb4e0510c35a6b31b0ef
Geass <geass1000@gmail.com>
2017-08-25 04:30:09 +0300

    Add: Controller for the generate user PDF file

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -5,7 +5,27 @@ export class UserController extends BaseController {
 		super();
 	}
 
-	updateUser () : void {
+	getUpdateUserPDF (req : express.Request, res : express.Response) : void {
+		const methodName : string = 'getUpdateUserPDF';
+		let message : string;
+
+		const firstName : string = req.params['first_name'].toString().trim();
+
+		userModel.findUser(firstName)
+			.then((user) => {
+				return userModel.generatePDF(user);
+			})
+			.then((pdf) => {
+				return userModel.updateUserPDF(firstName, pdf);
+			})
+			.then(() => {
+				message = 'PDF file was generated successfully'
+				this.sendSuccessResponse(res, 200, { status : true }, methodName, message);
+			})
+			.catch((error) => {
+				message = 'PDF file wasn\'t generated'
+				this.sendErrorResponse(res, error, { status : false }, methodName, message);
+			});
 	}
 }
 
